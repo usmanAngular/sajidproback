8bd90c42c2f9d6ac36ea809f29d2955080b36c14
frazoffice <frazmirzaoffice58@gail.com>
2022-02-16 01:39:07 +0500

    collect static

--- project_backend/settings.py
+++ project_backend/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Quick-start development settings - unsuitable for production
