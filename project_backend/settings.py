a45937bf791e0fe2ee057a3c44e1428171332762
Muhammad Usman Khan <35483640+usmankhan7779@users.noreply.github.com>
2022-02-26 19:40:16 +0500

    sads

--- project_backend/settings.py
+++ project_backend/settings.py
@@ -139,15 +139,14 @@ WSGI_APPLICATION = 'project_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',   # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 's_database',
-        'USER': 'ange_db',
+        'NAME': 'backend_project',
+        'USER': 'postgres',
         'port':'5432',
-        'PASSWORD': 'ange@321',                                # Not used with sqlite3.
+        'PASSWORD': 'admin12345',                                # Not used with sqlite3.
         'HOST': 'localhost',                                  # Set to empty string for localhost. Not used with sqlite3.
                                    # Set to empty string for default. Not used with sqlite3.
     }
 }
-
 
 
 # Password validation
